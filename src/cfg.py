91dc7ee2b000899b99c7438c97033068411dba1d
Sondelll <sondelln@gmail.com>
2023-01-31 14:22:25 +0100

    Extended config read to include model

--- src/cfg.py
+++ src/cfg.py
@@ -1,14 +1,15 @@
 import yaml
 
 
-
 def model_str() -> str:
     cfg = parse_yaml("./config/crokket.yml")
-    _size = cfg["model_size"]
-    return f"openai/whisper-{_size}"
+    if cfg['model'] == "w2v":
+        return "facebook/wav2vec2-large-960h-lv60-self"
+    elif cfg['model'] == "whisper":
+        _model = "openai/whisper-"
+        _size = cfg['model_size']
+        return f"{_model}{_size}"
     
-
-
 
 def parse_yaml(path:str):
     with open(path) as file:
