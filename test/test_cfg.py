804cc00f306775880d0871c091443ebefdff5ddf
Sondelll <sondelln@gmail.com>
2023-01-31 13:00:04 +0100

    Added test for model str from config

---
+++ test/test_cfg.py
@@ -0,0 +1,10 @@
+import os
+import sys
+sys.path.insert(0, os.curdir)
+
+from src.cfg import model_str
+
+def test_model_string():
+    m_s = model_str()
+    assert isinstance(m_s, str)
+    assert "openai/whisper" in m_s
