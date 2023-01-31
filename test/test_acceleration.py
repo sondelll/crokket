83ce8da8fbf34ffd4636ab4e933b1437c9bd84e6
Sondelll <sondelln@gmail.com>
2023-01-31 12:59:30 +0100

    Added acceleration detection test

---
+++ test/test_acceleration.py
@@ -0,0 +1,10 @@
+import os
+import sys
+sys.path.insert(0, os.curdir)
+
+import torch
+from src.transcription.acceleration import get_acceleration_device
+
+def test_acc_device_fetch():
+    a_device = get_acceleration_device()
+    assert isinstance(a_device, torch.device)
